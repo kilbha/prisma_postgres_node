9cb1c4e94fc374880e9ad8386dbbf2d6c3e44a74
kilbha <bhargav.mech12@gmail.com>
2023-09-20 23:00:46 +0530

    prims with postgress

--- prisma-postgres/src/index.ts
+++ prisma-postgres/src/index.ts
@@ -1,8 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
+import prisma from "./db/prisma";
 
 dotenv.config();
 
 const app = express();
+
+app.get("/", async (req, res) => {
+  await prisma.user.create({
+    data: {
+      name: "ramesh",
+      email: "johndoe@gmail.com",
+      password: "12345",
+    },
+  });
+
+  //Get all users
+  const users = await prisma.user.findMany();
+
+  //Get user names
+  const names = users.map((user) => user.name);
+
+  res.send(
+    `There are ${names.length} users with names of: ${names.join(", ")} `
+  );
+});
 
 app.listen(3000, () => console.log("server running"));
